ccfcdc24e55aee3481b325a495868c722a7e0f54
alungggggg <aldinoalungputraanugraha@gmail.com>
2024-03-03 20:59:02 +0700

    add middleware admin

--- routes/web.php
+++ routes/web.php
@@ -37,50 +37,50 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
     });
+
+    // article
+    Route::get('article', [ArticleController::class, 'show']);
+    Route::get('/article/add', [ArticleController::class, 'addView']);
+    Route::post('/article/add', [ArticleController::class, 'add']);
+    Route::get('/article/{id}', [ArticleController::class, 'detail']);
+    Route::get('/article/update/{id}', [ArticleController::class, 'updateView']);
+    Route::put('/article/update/{id}', [ArticleController::class, 'update']);
+
+
+    // umkm
+    Route::get('/umkm', [umkmController::class, 'show']);
+    Route::get('/umkm/add', function () {
+        return view('umkm.add');
+    });
+    Route::post('/umkm/add', [umkmController::class, 'add']);
+    Route::get('umkm/update/{id}', [umkmController::class, 'updateView']);
+    Route::put('umkm/update/{id}', [umkmController::class, 'update']);
+    Route::get('umkm/delete/{id}', [umkmController::class, 'delete']);
+
+    // lkk
+    Route::get('/lkk', [lkkController::class, 'show']);
+    Route::get('/lkk/add', function () {
+        return view('lkk.add');
+    });
+    Route::post('/lkk/add', [lkkController::class, 'add']);
+    Route::get('/lkk/update/{id}', [lkkController::class, 'updateView']);
+    Route::put('/lkk/update/{id}', [lkkController::class, 'update']);
+    Route::get('/lkk/delete/{id}', [lkkController::class, 'delete']);
+
+    // user
+    Route::get('/user', [userController::class, 'show']);
+    Route::get('/user/add', [UserController::class, 'addView']);
+    Route::post('/user/add', [userController::class, 'add']);
+    Route::get('/user/update/{id}', [userController::class, 'updateView']);
+    Route::put('/user/update/{id}', [userController::class, 'update']);
+    Route::get('/user/delete/{id}', [userController::class, 'delete']);
+
+    Route::get('/test', function () {
+        dd(Auth::user()->lkk_id);
+    });
 });
 
 Route::get('/testfeed', function () {
     return view('testfeed');
 });
 Route::post('/feedback', [FeedController::class, 'add'])->middleware('throttle:feedback');
-
-// article
-Route::get('article', [ArticleController::class, 'show']);
-Route::get('/article/add', [ArticleController::class, 'addView']);
-Route::post('/article/add', [ArticleController::class, 'add']);
-Route::get('/article/{id}', [ArticleController::class, 'detail']);
-Route::get('/article/update/{id}', [ArticleController::class, 'updateView']);
-Route::put('/article/update/{id}', [ArticleController::class, 'update']);
-
-
-// umkm
-Route::get('/umkm', [umkmController::class, 'show']);
-Route::get('/umkm/add', function () {
-    return view('umkm.add');
-});
-Route::post('/umkm/add', [umkmController::class, 'add']);
-Route::get('umkm/update/{id}', [umkmController::class, 'updateView']);
-Route::put('umkm/update/{id}', [umkmController::class, 'update']);
-Route::get('umkm/delete/{id}', [umkmController::class, 'delete']);
-
-// lkk
-Route::get('/lkk', [lkkController::class, 'show']);
-Route::get('/lkk/add', function () {
-    return view('lkk.add');
-});
-Route::post('/lkk/add', [lkkController::class, 'add']);
-Route::get('/lkk/update/{id}', [lkkController::class, 'updateView']);
-Route::put('/lkk/update/{id}', [lkkController::class, 'update']);
-Route::get('/lkk/delete/{id}', [lkkController::class, 'delete']);
-
-// user
-Route::get('/user', [userController::class, 'show']);
-Route::get('/user/add', [UserController::class, 'addView']);
-Route::post('/user/add', [userController::class, 'add']);
-Route::get('/user/update/{id}', [userController::class, 'updateView']);
-Route::put('/user/update/{id}', [userController::class, 'update']);
-Route::get('/user/delete/{id}', [userController::class, 'delete']);
-
-Route::get('/test', function () {
-    dd(Auth::user()->lkk_id);
-});
